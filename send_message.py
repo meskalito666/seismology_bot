903c4d63700dcc65d2c7d2b1522ec6cb6719419d
martirosov.e <sweet.meskalito@gmail.com>
2023-02-12 15:43:21 +0400

    refactor: moved format_string() to data_handler.py

--- send_message.py
+++ send_message.py
@@ -1,39 +1,6 @@
 import requests
-from typing import Dict
 from consts import TOKEN_TG_BOT, TG_API_URL, CHANEL_ID
-from data_handler import get_location_name, get_map_link, get_time
 
-
-
-def format_string(info: Dict) -> str:
-    # get type of action
-    if info['action'] == 'create':
-        action = 'New earthquake! \n'
-    elif info['action'] == 'update':
-        action = 'Update. \n'
-    else:
-        action = ''
-        
-    # get magnitude
-    if info['mag']:
-        mag = f'mag: {info["mag"]} \n'
-    else:
-        mag = ''
-
-    # get time from json
-    # if info['time']:
-    #     time = f'time: {info["time"]} \n'
-    # else:
-    #     time = ''
-
-    # get Armeninan local time
-    time = get_time()
-    # get location
-    lacation_name = get_location_name(info['location'][1], info['location'][0])
-    # get map link
-    map_link = get_map_link(info['location'][1], info['location'][0])
-
-    return action + time + mag + lacation_name + map_link
 
 
 def send_message(message: str) -> None:
@@ -48,8 +15,8 @@ def send_message(message: str) -> None:
         "parse_mode": "HTML"
         })
     
-    # if r.status_code != 200:
-    #     raise Exception("post_text error")
+    if r.status_code != 200:
+        print("Error sending message")
 
 if __name__ == '__main__':
     send_message('test msg')
