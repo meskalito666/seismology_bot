50bb363ae8b075f1d1478983ad9600d1e76b41f3
martirosov.e <sweet.meskalito@gmail.com>
2023-02-14 12:59:15 +0400

    refactor: added chat_id as argument

--- send_message.py
+++ send_message.py
@@ -3,14 +3,14 @@ from consts import TOKEN_TG_BOT, TG_API_URL, CHANEL_ID
 
 
 
-def send_message(message: str) -> None:
+def send_message(message: str, chat_id: str=CHANEL_ID) -> None:
     url = TG_API_URL + TOKEN_TG_BOT
     method = url + "/sendMessage"
 
     r = requests.post(
        method, 
        data={
-        "chat_id": CHANEL_ID,
+        "chat_id": chat_id,
         "text": message,
         "parse_mode": "HTML"
         })
