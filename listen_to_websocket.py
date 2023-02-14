0810168d3b65c6ccdef8d9339f4578bec0c60c0a
martirosov.e <sweet.meskalito@gmail.com>
2023-02-14 13:04:55 +0400

    features: 1. additiona cheker for timediff between updates 2. context manager to notify the admin in case of error or long delay

--- listen_to_websocket.py
+++ listen_to_websocket.py
@@ -1,10 +1,12 @@
 import json
+import asyncio
 from tornado.websocket import websocket_connect
 from tornado.ioloop import IOLoop
 from tornado import gen
 from typing import Generator
-from data_handler import check_location, prepare_msg_for_tg
+from data_handler import check_location, check_time, prepare_msg_for_tg
 from send_message import send_message
+from shutdown_alert import shutdown_alert
 from consts import WEB_SOCKET_URL, PING_INTERVAL, TEST_MODE
 
 
@@ -15,14 +17,14 @@ def processing(message: str) -> None:
         info = data['data']['properties']
         info['action'] = data['action']
         longitude, latitude, _ = data['data']['geometry']['coordinates']
-        
+
         if TEST_MODE == 'test':
             # send notifications from all over the world (earthquakes happen every ~5 minutes)
             tg_message = prepare_msg_for_tg(latitude, longitude, info)
             send_message(tg_message)
         else:
             # send notifications only from Armenia (and from a small area around)
-            if check_location(latitude, longitude, info['flynn_region']):
+            if check_location(latitude, longitude, info['flynn_region']) and check_time(info):
                 tg_message = prepare_msg_for_tg(latitude, longitude, info)
                 send_message(tg_message)
 
@@ -32,12 +34,15 @@ def processing(message: str) -> None:
 
 @gen.coroutine
 def listen(ws) -> Generator:
-    while True:
-        message = yield ws.read_message()
-        if message is None:
-            ws = None
-            break
-        processing(message)
+    with shutdown_alert() as sa:
+        while True:
+            time_cheker = asyncio.create_task(sa.timer()) #timer started
+            message = yield ws.read_message() #listening to socket
+            if message is None:
+                ws = None
+                break
+            processing(message)
+            time_cheker.cancel() # cancel task in order to restart timer 
 
 
 @gen.coroutine
@@ -60,3 +65,4 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         print("Close WebSocket")
         ioloop.stop()
+
